f314c4a0f393b15233318cf639214ab2a36ad791
shagufta <shagufta.batool8@gmail.com>
2021-11-16 19:49:47 +0100

    atm challenge happy path use case

--- lib/atm.rb
+++ lib/atm.rb
@@ -1,9 +1,36 @@
+require "date"
 
 class Atm
+  attr_accessor :funds
 
-    attr_accessor :funds
+  def initialize
+    @funds = 1000
+  end
 
-    def initialize
-        @funds = 1000
+  def withdraw(amount, account)
+    # @funds -= value
+    case
+    when insufficient_funds_in_account?(amount, account)
+      return
+    else
+      perform_transaction(amount, account)
     end
+  end
+
+  private
+
+  def insufficient_funds_in_account?(amount, account)
+    amount > account.balance
+  end
+
+  def perform_transaction(amount, account)
+    @funds -= amount
+    account.balance = account.balance - amount
+    {
+      status: true,
+      message: "success",
+      date: Date.today,
+      amount: amount,
+    }
+  end
 end
--- spec/atm_spec.rb
+++ spec/atm_spec.rb
@@ -1,9 +1,27 @@
-
-require './lib/atm.rb'
+require "./lib/atm.rb"
 
 describe Atm do
-    subject {Atm.new}
-    it 'is expected to hold $1000 when instantiated' do
-     expect(subject.funds).to eq 1000
-end
+  subject { Atm.new }
+  let (:account) { instance_double("Account") }
+  before do
+    allow(account).to receive(:balance).and_return(100)
+    allow(account).to receive(:balance=)
+  end
+  it "is expected to hold $1000 when instantiated" do
+    expect(subject.funds).to eq 1000
+  end
+  it "is expected to reduce funds on withdraw" do
+    subject.withdraw 50, account
+    expect(subject.funds).to eq 950
+  end
+  it "is expected to allow withdrawal if account has enough balance" do
+    expected_output = {
+      status: true,
+      message: "success",
+      date: Date.today,
+      amount: 45,
+    }
+
+    expect(subject.withdraw(45, account)).to eq expected_output
+  end
 end
